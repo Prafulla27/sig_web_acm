64de3114131b77b5bfad8c84dfeda2ac9d102325
Prafulla27 <92382416+Prafulla27@users.noreply.github.com>
2022-05-14 19:21:57 +0530

    task9

---
+++ task9/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+//create a folder
+//fs.mkdirSync("CURD");
+
+//create a file and write 
+//fs.writeFileSync("CURD/curd.txt", "This is SIG WEB Task 9");
+
+//update a file
+//fs.appendFileSync("CURD/curd.txt", " Learning to perform curd operations");
+
+//read data of file
+//const data = fs.readFileSync("CURD/curd.txt","utf8");
+//console.log(data);
+
+//delete file
+//fs.unlinkSync("CURD/curd.txt");
+
+//remove folder
+//fs.rmdirSync("CURD");
